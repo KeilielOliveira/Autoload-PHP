f3906bd994afdcc79c9dacac3a4c69c2625f24f1
Keiliel Oliveira <keilieloliveira.2004@gmail.com>
2024-05-23 08:58:11 -0300

    V 1.1.0
    Adicionado
    
     - Aceita registro de namespaces atrvés de um array asscociativo.
     - Aceita registro de namespaces atrvés de um arquivo JSON.
     - Aceita tentar carregar classes que não foram registradas.

--- index.php
+++ index.php
@@ -4,12 +4,6 @@ use Autoload\Autoload;
 
 require('src/Autoload.php');
 
-new Autoload([
-    'Testes' => 'src/'
-], [
-    'load_undefined_class' => true
-]);
-
-new Src\ClasseDeTestes;
+new Autoload('autoload.json');
 
 ?>
--- src/Autoload.php
+++ src/Autoload.php
@@ -2,23 +2,30 @@
 
 namespace Autoload;
 
-use Exception;
-
 class Autoload {
     
     protected static $namespaces;
     protected static $config;
     protected static $autoloads;
 
-    public function __construct(array $namespaces, array $config = []) {
+    public function __construct(array | string $namespaces, array $config = []) {
         
+        //Se os namespaces do autload forem passados em um arquivo json.
+        if(gettype($namespaces) == 'string') {
+            //Se os namespaces forem passados como string.
+            
+            if(file_exists($namespaces)) {
+                $namespaces = json_decode(file_get_contents($namespaces), true);
+            }
+        }
+
+        //Salva as informações uteis.
         self::$autoloads = ['loader', 'loadUndefinedClass'];
         self::$namespaces = $namespaces;
         self::$config = $config;
 
         //Registra todos os metodos de autoload dinamicamente.
         foreach (self::$autoloads as $key => $autoload) {
-            
             spl_autoload_register([__CLASS__, $autoload]);
         }
     }
@@ -27,7 +34,6 @@ class Autoload {
      * Carrega a classe se ela for encontrada.
      */
     protected function loader($namespace) {
-
         $namespace = explode('\\', $namespace);
         $class = array_pop($namespace);
         $namespace = implode('\\', $namespace);
@@ -40,31 +46,29 @@ class Autoload {
             $file = str_replace('\\', DIRECTORY_SEPARATOR, $filename); //Monta o nome do arquivo.
             if(file_exists($file)) {
                 //Se o arquivo existir.
-
                 require($file);
                 return true;
             }
         }
     }
 
+    /**
+     * Tenta carregar classes que não foram registradas.
+     */
     protected function loadUndefinedClass($namespace) {
-
         if(isset(self::$config['load_undefined_class']) && self::$config['load_undefined_class']) {
             //Se foi permitido a tentativa de carregamento de classes não registradas.
 
             $namespace = str_replace('\\', DIRECTORY_SEPARATOR, $namespace);
             $file = $namespace . '.php';
-
             if(file_exists($file)) {
                 //Se o arquivo for encontrado.
-
                 require($file);
                 return true;
             }
 
         }
     }
-
 }
 
 ?>
